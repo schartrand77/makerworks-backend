8071b581a72f6965eeb3e0cba3947af4547b6bac
schartrand77 <60296853+schartrand77@users.noreply.github.com>
2025-07-17 09:46:52 -0400

    🚀 Replace entire repo with latest local version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 cd /app
 
-exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app
+# Listen on 0.0.0.0 so it’s reachable from host
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 app.main:app
